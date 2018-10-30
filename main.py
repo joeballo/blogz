189224f32d87ffa0075dc4d9e744f8e7af9bfa6c
Joseph Ballard <joeballo@gmail.com>
2018-10-30 05:54:05 -0500

    first steps complete in build a blog assignment

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8888/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -14,32 +14,33 @@ class Blog(db.Model):
     blog_title = db.Column(db.String(120))
     blog_body = db.Column(db.String(300))
 
-    def __init__(self, blog_title):
+    def __init__(self, blog_title, blog_body):
         self.blog_title = blog_title
-
-
-
-@app.route('/') #first visit
-def index():
-
-    return render_template('add-new-blog.html')
-
-
-@app.route('/newpost', methods=['POST', 'GET']) #create a blog
-def index():
-
-
-    db.session.add()
-    db.session.commit()
-    
-    return render_template('add-new-blog.html')
-
+        self.blog_body = blog_body
 
 @app.route('/blog') # view blogs
 def blog_listing():
 
+    blogs = Blog.query.all()
+    return render_template("main-blog-listings.html", blogs=blogs)
 
-    return render_template ('main-blog-listing.html')
+
+@app.route('/newpost', methods=['POST', 'GET']) #create a blog
+def new_post():
+
+    if request.method == 'POST':
+        blog_title = request.form['blog_title']
+        blog_body = request.form['blog_body']
+        new_blog = Blog(blog_title, blog_body)
+        db.session.add(new_blog)
+        db.session.commit()
+
+    blogs = Blog.query.all()
+
+    return render_template('add-new-blog.html', blogs=blogs)
+
+
+
 
 
 
