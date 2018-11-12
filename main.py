342be23fcfe5f70c11d0a37faa19bfd105b16b6e
Joseph Ballard <joeballo@gmail.com>
2018-11-12 03:25:48 -0600

    updates to hashing

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30))
-    pw_hash = db.Column(db.String(30))
+    pw_hash = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, username, password):
